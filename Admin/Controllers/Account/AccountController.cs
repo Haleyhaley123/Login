b5ad93e1274dc07e8a7e9d08c3972cfbb67e1c80
halt63 <halt63@vingroup.net>
2022-09-22 14:39:14 +0700

    update

---
+++ Admin/Controllers/Account/AccountController.cs
@@ -0,0 +1,92 @@
+﻿using Domain.BaseModel;
+using Domain.User;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using Reponsitory.Interface;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using WebDemo.Constants;
+using static Domain.DBContext.DBContext;
+
+namespace Admin.Controllers.Account
+{
+    public class AccountController : Controller
+    {
+        public ActionResult Login()
+        {
+            return View();
+        }
+        private readonly IUserReponsitory _iUserReponsitory;
+        private readonly IConfiguration _configuration;
+        private readonly DatabaseContext _dataBaseContext;
+        public AccountController(
+            IUserReponsitory iUserService, IConfiguration configuration, DatabaseContext dataBaseContext
+            )
+        {
+            _configuration = configuration;
+            _iUserReponsitory = iUserService; ;
+            _dataBaseContext = dataBaseContext;
+        }
+       
+        [HttpPost]
+        public async Task<ActionResult<WsResponse>> LoginAccount(string username, string password)
+        {
+            WsResponse response = new WsResponse();
+            var data = await _iUserReponsitory.LoginAccount(username, password);
+            if (data != null)
+            {
+                var token = "";
+                var rolecode = "abcd";
+                try
+                {
+                    var authClaims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, data.UserName),
+                    new Claim(ClaimTypes.Role, rolecode),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                };
+                    if (data.RoleCode != null)
+                    {
+                        rolecode = data.RoleCode.ToString();
+                        authClaims.Add(new Claim(ClaimTypes.Role, rolecode));
+                    }
+                    token = CreateToken(data);
+                }
+                catch
+                {
+
+                }
+
+                response.Data = token;
+                response.Status = WsConstants.MessageLoginSuccess;
+            }
+            else
+            {
+
+                response.Status = WsConstants.MessageLoginFaild;
+
+            }
+            return View(response);
+        }
+        private string CreateToken(UserModel data)
+        {
+            List<Claim> claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name, data.UserName),
+            };
+            var secretKey = "mysupersecret_secretkey!123";
+            var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(secretKey));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.Now.AddHours(2),
+                signingCredentials: creds);
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+            data.Token = jwt;
+            _dataBaseContext.Users.Update(data);
+            _dataBaseContext.SaveChangesAsync();
+
+            return jwt;
+        }
+    }
+}
--- Admin/Program.cs
+++ Admin/Program.cs
@@ -1,14 +1,49 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Reponsitory.Interface;
+using Reponsitory.Reponsitory;
+using System.Text;
+using static Domain.DBContext.DBContext;
+
 var builder = WebApplication.CreateBuilder(args);
-
+ConfigurationManager configuration = builder.Configuration;
 // Add services to the container.
-builder.Services.AddRazorPages();
-
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<DatabaseContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+    .AddJwtBearer(options =>
+    {
+        options.SaveToken = true;
+        options.RequireHttpsMetadata = false;
+        options.TokenValidationParameters = new TokenValidationParameters()
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidAudience = configuration["JWWT:ValidAudience"],
+            ValidIssuer = configuration["JWWT:ValidIssuer"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Secret"]))
+        };
+    });
+builder.Services.AddDbContext<DatabaseContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+builder.Services.AddScoped<IUserReponsitory, UserReponsitory>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
+    app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
@@ -20,6 +55,8 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapRazorPages();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
